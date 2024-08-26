55658dc014272f4e90867685d4c51a488c3e7445
sothulthorn <thornsothul@gmail.com>
2024-08-26 21:52:51 +0900

    API Documentation configuring with swagger

--- chatify/settings.py
+++ chatify/settings.py
@@ -30,9 +30,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    
+    # Internal
     "account",
     "server",
+    # External
+    "drf_spectacular"
 ]
 
 MIDDLEWARE = [
@@ -119,4 +121,15 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "account.Account"
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Chatify API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': True,
+}
 
